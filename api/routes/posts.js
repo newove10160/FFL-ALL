962110033758a15678491ce607e2d1cbe53d2452
Memorisza <aut14030@hotmail.com>
2021-12-10 16:50:14 +0700

    [UPDATE] Post Router

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,7 +7,7 @@ router.post("/", async (req, res)=>{
     const newPost = new Post(req.body)
     try {
         const savedPost = await newPost.save();
-        res.status(200).json(savedPost);
+        res.status(201).json(savedPost);
     } catch (error) {
         res.status(500).json(err);
     }
